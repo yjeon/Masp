c7cfba3417ac4354e2a86a42ec667bf66cd24c96
Yoonchan <yjeon@masonlive.gmu.edu>
2016-09-26 16:08:59 -0400

    edit firebase init

--- server_signin.js
+++ server_signin.js
@@ -1,33 +1,23 @@
 (function(){
 
-    // Initialize firebase
-    // const config = { 
-    // apiKey: "AIzaSyCuWAvUnbjSAGD7XqansTe2tUoqPORncl0",
-    // authDomain: "masp-9a79d.firebaseapp.com",
-    // databaseURL: "https://masp-9a79d.firebaseio.com",
-    // storageBucket: "masp-9a79d.appspot.com",
-    // messagingSenderId: "686393566018"
+    Initialize firebase
+    const config = { 
+    apiKey: "AIzaSyCuWAvUnbjSAGD7XqansTe2tUoqPORncl0",
+    authDomain: "masp-9a79d.firebaseapp.com",
+    databaseURL: "https://masp-9a79d.firebaseio.com",
+    storageBucket: "masp-9a79d.appspot.com",
+    messagingSenderId: "686393566018"
 
-    // }; 
+    }; 
 
-    // firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
     // HTML elements
     const txtEmail    = document.getElementById('txtEmail');
     const txtPassword = document.getElementById('txtPassword');
-    //const btnLogIn    = document.getElementById('btnLogIn');
     const btnSignUp   = document.getElementById('btnSignUp');
-    //const btnLogOut   = document.getElementById('btnLogOut');
 
-    // 	//add login event
-    // 	btnLogIn.addEventListener('click', e => {
-    //     	//get email and pass
-    // 		const email = txtEmail.value;
-    // 		const pass = txtPassword.value;
-    // 		const auth = firebase.auth();
-    // const promise = auth.signInWithEmailAndPassword(email,pass);
-    // promise.catch(e => console.log(e.message));
-    // 	});
+
 
     //Add Sign up
     btnSignUp.addEventListener('click', e => {
@@ -40,19 +30,4 @@
 
     });
 
-    // btnLogOut.addEventListener('click', e => {
-    // 	firebase.auth().signOut();
-    // });
-
-
-    // firebase.auth().onAuthStateChanged(firebaseUser => {
-        // 	if(firebaseUser){
-        // 		console.log(firebaseUser);
-        // 		btnLogOut.classList.remove('hide');
-        // 	}
-        // 	else{
-        // 		console.log('not logged in');
-        // 		btnLogOut.classList.add('hide');
-        // 	}
-    // });
 }());
