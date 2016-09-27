9b29a882c9308ad186e73cc10fa56be88de56072
iamroot <iamroot@mbp.local>
2016-09-27 01:01:10 -0400

    test

--- js/server.js
+++ js/server.js
@@ -18,9 +18,7 @@
     btnLogIn.addEventListener('click', e => {
         const email   = txtEmail.value;
         const pass    = txtPassword.value;
-        const auth    = firebase.auth();
-        const promise = auth.signInWithEmailAndPassword(email,pass);
-        promise.catch(validate(error) {
+        firebase.auth().signInWithEmailAndPassword(email,pass).catch(function(error) {
             throw 'not working';
         });
     });
