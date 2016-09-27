32011ba716d91132373e0a05468f7f234b13e25f
iamroot <iamroot@mbp.local>
2016-09-27 01:13:08 -0400

    test

--- js/login.js
+++ js/login.js
@@ -10,15 +10,9 @@ function removeSignup() {
 }
 */
 
-/*
 $(function(){
 	$('#loginform').submit(function(e){
 		return false;
 	});
 	$('#modaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
 });
-
-function authLogin() {
-
-}
-*/
