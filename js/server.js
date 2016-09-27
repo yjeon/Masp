45e1c1f7d73ff1e8f7ed7b824a41a979b41de805
iamroot <iamroot@mbp.local>
2016-09-27 09:29:26 -0400

    test

--- js/server.js
+++ js/server.js
@@ -2,13 +2,12 @@
 
     // Initialize firebase
     const config = { 
-    apiKey: "AIzaSyCuWAvUnbjSAGD7XqansTe2tUoqPORncl0",
-    authDomain: "masp-9a79d.firebaseapp.com",
-    databaseURL: "https://masp-9a79d.firebaseio.com",
-    storageBucket: "masp-9a79d.appspot.com",
-    messagingSenderId: "686393566018"
-
-    }; 
+        apiKey: "AIzaSyCuWAvUnbjSAGD7XqansTe2tUoqPORncl0",
+        authDomain: "masp-9a79d.firebaseapp.com",
+        databaseURL: "https://masp-9a79d.firebaseio.com",
+        storageBucket: "masp-9a79d.appspot.com",
+        messagingSenderId: "686393566018"
+    };
 
     firebase.initializeApp(config);
 
@@ -19,7 +18,7 @@
     const btnSignUp   = document.getElementById('btnSignUp');
     const btnLogout   = document.getElementById('btnLogOut');
 
-     	//add login event
+    //add login event
     loginbtn.addEventListener('click', e => {
     	//get email and pass
         const email = txtEmail.value;
@@ -29,15 +28,6 @@
         promise.catch(e => console.log(e.message));
 
     });
-
-    // //Add Sign up
-    // btnSignUp.addEventListener('click', e => {
-    //     const email = txtEmail.value;
-    //     const pass = txtPassword.value;
-    //     const auth = firebase.auth();
-    //     const promise = auth.createUserWithEmailAndPassword(email,pass);
-    //     promise.catch(e => console.log(e.message));
-    // });
 
     btnLogOut.addEventListener('click', e => {
      	firebase.auth().signOut();
@@ -50,19 +40,12 @@
             console.log("here we go!");
             console.log(firebaseUser.email);
     		btnLogOut.classList.remove('hide');
-            
-            
-
     	}
     	else{
     		console.log('not logged in');
     		btnLogOut.classList.add('hide');
             
     	}
-            
-        
-
-
     });
 }());
 
