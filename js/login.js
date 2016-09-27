32e0025f0667ae835896d273f330adda139d16bd
iamroot <iamroot@mbp.local>
2016-09-27 09:16:19 -0400

    test

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 document.getElementById("loginbtn").addEventListener("click", removeSignup);
 function removeSignup() {
     var signup = document.getElementById("signup");
-    signup.style.visibility = "hidden";
+     signup.style.visibility = "hidden";
     document.getElementById("modaltrigger").innerHTML = document.getElementById("");
     var logout = document.getElementById("btnLogOut");
     logout.style.display = "block";
@@ -13,4 +13,9 @@ $(function(){
 		return false;
 	});
 	$('#modaltrigger').leanModal({ top: 110, overlay: 0.45, closeButton: ".hidemodal" });
+
 });
+
+
+
+
